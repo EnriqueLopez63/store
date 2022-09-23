c098e2c3a784679df08541e9a697592abf65ef84
EnriqueLopez63 <111899612+EnriqueLopez63@users.noreply.github.com>
2022-09-23 09:35:32 -0700

    Add files via upload

--- store.py
+++ store.py
@@ -20,12 +20,12 @@ def storeMeth():
 
     womensClothes4 = {'Somerset Maxidress Anthropologie in white':168 , 'Balenciaga Womens flower jersey Cycling dressL':3190 , 'Tu Lize knitted sleeves oversized jacket':1086 , 'Dior mid lengthe shirt dress yellow silk chiffon multi color pixel zodiac':5000 , 'Womens White Auburn Dress':433.00 , 'Black top with stars':98 , 'Old couch pattern mini dress':75.99}
 
-    tech5 = {'Meta Quest 2 Vr':399.99  , 'Acer Swift laptop':1099.99 , 'Flagship chromebook':94.99, "Google pixel 6a-$369.99" , "Samsung Galaxy A10e-$102.99" , "Iphone 11pro-$524.95" , "Intel i7cpu-$321.97" , "Hp pavillio32-$2,399.99" , "Lphone 10-$5" , "Samsung odyysey ark monitor-$3,499.99"]
+    tech5 = {'Meta Quest 2 Vr':399.99  , 'Acer Swift laptop':1099.99 , 'Flagship chromebook':94.99, 'Google pixel 6a':369.99 , 'Samsung Galaxy A10e':102.99 , 'Iphone 11pro':524.95 , 'Intel i7cpu':321.97 , 'Hp pavillio32':2399.99 , 'Lphone 10':5 , 'Samsung odyysey ark monitor':3499.99}
 
-    bakery6 = ["GlazedDonut-$.99" , "ChocolateDonut-$.99" , "LoafofBread-$4.50" , "Sugar poudered poudered sugar that is poudered donuts-$41.97" , "ChocolateChipCookie-$1.25" , "SugarCookie-$1"
-    , "Brownie-$1.25" , "Pizza-$12" , "Cake-$9" , "Cupcake-$.75"]
+    bakery6 = {'GlazedDonut':99 , 'ChocolateDonut':.99 , 'LoafofBread':4.50 , 'Sugar poudered poudered sugar that is poudered donuts':41.97 , 'ChocolateChipCookie':1.25 , 'SugarCookie':1
+    , 'Brownie':1.25 , 'Pizza':12 ,'Cake':9 , 'Cupcake':.75}
 
-    beauty7 = ["red lipstick-$7" , "BlueLipstick-$5" , "orangeLipstick-$4" , "pinkLipstick-7" , "Mascara-$20" , "LashExtensions-$15" , "NoseSurgery-$5,000" , "greenWig-$25" , "deoderant-$2" "Shampoo-$10"] 
+    beauty7 = {'red lipstick':7 , 'BlueLipstick':5 , 'orangeLipstick':4, 'pinkLipstick':7 , 'Mascara':20 , 'LashExtensions':15 , 'NoseSurgery':5000 , 'greenWig':25 , 'deoderant':2 , 'Shampoo':10} 
 
     while (cont == "y"):
 
@@ -48,24 +48,26 @@ def storeMeth():
 
 
         elif int(store) == 3:
-            for item in mensClothes3:
-                print(item)
+            for item , value3 in mensClothes3.items():
+                print(item , value3 )
 
 
         elif int(store) == 4:
-            for item in womensClothes4:
-                print(item)
+            for item , value4 in womensClothes4.items():
+                print(item , value4)
 
 
         elif int(store) == 5:
-            for item in tech5:
-                print(item)
+            for item , value5 in tech5.items():
+                print(item , value5)
 
         elif int(store) == 6:
-            print(bakery6)
+            for item , value6 in bakery6.items():
+                print(item , value6)
 
         elif int(store) == 7:
-            print(beauty7)
+            for item , value7 in beauty7.items():
+                print(item ,value7)
 
         inue = input("would you like to visit another section please type y or n")
         while inue != "y" and inue != "n":
